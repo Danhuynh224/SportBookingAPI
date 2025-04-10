25c5fe60a9c3dc31d397a63c1d5bab646aa263a7
nguyentri61 <nguyenphanminhtri612004@gmail.com>
2025-04-10 16:58:18 +0700

    Thêm savePost

--- src/main/java/com/example/apiproject/controller/PostController.java
+++ src/main/java/com/example/apiproject/controller/PostController.java
@@ -4,9 +4,7 @@ import com.example.apiproject.entity.Post;
 import com.example.apiproject.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,11 @@ public class PostController {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(posts);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Post> savePost(@RequestBody Post post) {
+        Post savedPost = postService.savePost(post);
+        return ResponseEntity.ok(savedPost);
     }
 }
--- src/main/java/com/example/apiproject/service/PostService.java
+++ src/main/java/com/example/apiproject/service/PostService.java
@@ -15,4 +15,8 @@ public class PostService {
     public List<Post> getAllPosts() {
         return postRepository.findAll();
     }
+
+    public Post savePost(Post post) {
+        return postRepository.save(post);
+    }
 }
